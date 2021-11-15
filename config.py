0a207995f10d81a442c4ee907660537b273bcc75
simonkairu <simon.mureithi@student.moringaschool.com>
2021-11-15 16:39:58 +0300

    heroku deployment

--- config.py
+++ config.py
@@ -23,7 +23,8 @@ class ProdConfig(Config):
     Production configuration class
     """
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+      SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     
 class DevConfig(Config):
     """
